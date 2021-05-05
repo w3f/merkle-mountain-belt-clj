69a0b8a0f61ed9eba38d17d75b2b57ed4ce1646d
Robert Hambrock <roberthambrock@gmail.com>
2021-05-05 13:44:17 +0200

    format buffer

--- src/core.clj
+++ src/core.clj
@@ -2,9 +2,7 @@
   (:require [rhizome.viz :as viz]
             [clojure.spec.alpha :as s]
             [clojure.spec.gen.alpha :as sgen]
-            [clojure.spec.test.alpha :as stest]
-            )
-  )
+            [clojure.spec.test.alpha :as stest]))
 
 (def index (atom -1))
 
@@ -54,9 +52,7 @@
 (defn hash-node [node]
   (if (has-children? node)
     (hash (str (::left node) (::right node)))
-    (hash (::value node))
-    )
-  )
+    (hash (::value node))))
 
 (defn take-index []
   (swap! index inc))
@@ -85,8 +81,7 @@
   (if (has-children? node)
     (+ 1
        (apply max (map mmr-depth (children node))))
-    1
-    ))
+    1))
 
 (defn storage-add! [node]
   (swap! storage assoc-in [(hash-node node)] node))
@@ -106,33 +101,29 @@
 (defn mmr-leafcount [node]
   (if (has-children? node)
     (apply + (map mmr-leafcount (children node)))
-    1
-    ))
+    1))
 
 (defn is-power-of-two [num]
   (=
    (.pow (BigInteger. "2") (int (/ (Math/log num) (Math/log 2))))
-   num)
-  )
+   num))
 
 (defn mmr-append-leaf [old-node leaf]
   (if
-      (is-power-of-two (mmr-leafcount old-node))
+   (is-power-of-two (mmr-leafcount old-node))
+    ;; if number of leafs stemming from old-node is power of two, then create a new node that has the old-node as left child and the new leaf as right child
     (node old-node leaf (take-index))
-    (do (decrease-index) (node (::left old-node) (mmr-append-leaf (::right old-node) (assoc leaf ::value @index)) (take-index)))
-    ))
+    (do (decrease-index) (node (::left old-node) (mmr-append-leaf (::right old-node) (assoc leaf ::value @index)) (take-index)))))
 
 (defn mmr-from-leafcount [leafcount]
   (reset! index -1)
   (reduce (fn [root _]
             (mmr-append-leaf root (leaf (take-index))))
           (leaf (take-index))
-          (range (dec leafcount)))
-  )
+          (range (dec leafcount))))
 
 (defn mmr-graph [root]
-  (apply merge (flatten [ {(::value root) (map ::value (children root))} (map mmr-graph (children root)) ]))
-  )
+  (apply merge (flatten [{(::value root) (map ::value (children root))} (map mmr-graph (children root))])))
 
 (defn find-subtree [root node-key]
   (if (= (::value root) node-key)
@@ -142,20 +133,16 @@
        (flatten
         (filter
          #(not (or (nil? %) (empty? %)))
-         (map #(find-subtree % node-key) (children root)))))
-      )))
+         (map #(find-subtree % node-key) (children root))))))))
 
 (find-subtree (mmr-from-leafcount 9) "(1⋁2)⋁(3⋁4)")
 
-(let [
-      mmr (mmr-from-leafcount 11)
-      graph (mmr-graph mmr)
-      ]
+(let [mmr (mmr-from-leafcount 11)
+      graph (mmr-graph mmr)]
   (viz/view-graph (keys graph) graph
                   ::node->descriptor (fn [n] {::label n})
                   ;; ::cluster->descriptor (fn [n] {::label n})
-                  ::node->cluster (fn [node-key] (mmr-depth (find-subtree mmr node-key)))
-                  )
+                  ::node->cluster (fn [node-key] (mmr-depth (find-subtree mmr node-key))))
   ;; graph
   )
 
@@ -164,8 +151,7 @@
   (mmr-leafcount (::right (mmr-from-leafcount 14)))
 
   (mmr-leafcount (::left (::left (mmr-from-leafcount 14))))
-  (mmr-leafcount (::right (::left (mmr-from-leafcount 14))))
-  )
+  (mmr-leafcount (::right (::left (mmr-from-leafcount 14)))))
 
 (def example-mmr
   (do
@@ -173,7 +159,6 @@
     (node
      (node (leaf "a") (leaf "b") (take-index))
      (node (leaf "c") (leaf "d") (take-index))
-     (take-index)))
-  )
+     (take-index))))
 
 (def extended-mmr (reduce (fn [root leaf-label] (mmr-append-leaf root (leaf leaf-label))) example-mmr ["e" "f" "g" "h"]))
