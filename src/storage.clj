2549a0f94077c8f9270484dad9223a1e934307e5
Robert Hambrock <roberthambrock@gmail.com>
2021-08-17 03:31:40 +0200

    Reorder sexps for linear evaluation

--- src/storage.clj
+++ src/storage.clj
@@ -3,6 +3,24 @@
 (def storage-array (atom '[]))
 (def leaf-count (atom 0))
 
+(defn leaf-location [n]
+  (+ (* 2 n) 1))
+
+(defn peak-location [n]
+  (+ (* 2 n) 2))
+
+(defn highest-exponent-st-dividing [p n]
+  (last
+   (filter #(= 0.0
+               (mod (Math/abs n) (Math/pow p %)))
+           (range 0 (Math/abs n)))))
+
+(defn p-adic-order [p n]
+  (if (= 0 n)
+    ##Inf
+    (highest-exponent-st-dividing p n)
+    ))
+
 (defn add-internal [item index]
   (let [array-len (count @storage-array)
         ;; incidentally correct since index is calculated starting at 1 in lieu of 0
@@ -30,18 +48,6 @@
 
 ;; [0 0 0 1 0 2 x 3 0 4 x 5 x 6 x 7 0 8 x 9 x]
 
-(defn highest-exponent-st-dividing [p n]
-  (last
-   (filter #(= 0.0
-               (mod (Math/abs n) (Math/pow p %)))
-           (range 0 (Math/abs n)))))
-
-(defn p-adic-order [p n]
-  (if (= 0 n)
-    ##Inf
-    (highest-exponent-st-dividing p n)
-      ))
-
 (map #(p-adic-order % 3) (range 1 500))
 
 (defn parent-index [child-index]
@@ -53,11 +59,5 @@
 (= 12 (parent-index 6))
 (= 12 (parent-index 10))
 
-(defn leaf-location [n]
-  (+ (* 2 n) 1))
 
-(defn peak-location [n]
-  (+ (* 2 n) 2))
 
-(map leaf-location (range 100))
-(map peak-location (range 100))
