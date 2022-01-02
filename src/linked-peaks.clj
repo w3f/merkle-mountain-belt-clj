cf8ba432115d20c6869dd010ad9e8c434750f941
Robert Hambrock <roberthambrock@gmail.com>
2022-01-02 13:52:02 +0100

    rename for conceptual clarity

--- src/linked-peaks.clj
+++ src/linked-peaks.clj
@@ -1,18 +1,34 @@
 (ns linked-peaks)
 
-(defn peak-node [left height hash parent]
+(defn internal-node [left height hash parent]
   {:left left
    :height height
    :hash hash
-   :parent parent})
+   :parent parent
+   :type :peak})
+
+(defn peak-node [left height hash]
+  {:left left
+   :height height
+   :hash hash
+   :parent nil
+   :type :peak})
 
 (defn range-node [left right hash parent]
   {:left left
    :right right
    :hash hash
-   :parent parent})
+   :parent parent
+   :type :range})
 
-(def lastP (atom (peak-node nil 0 nil nil)))
+(defn belt-node [left right hash parent]
+  {:left left
+   :right right
+   :hash hash
+   :parent parent
+   :type :belt})
+
+(def lastP (atom (peak-node nil 0 nil)))
 
 ;; (def R-count (atom 0))
 
@@ -20,8 +36,8 @@
 
 (def leaf-count (atom 0))
 
-(def peak-map (atom {}))
-(def peak-array (atom []))
+(def node-map (atom {}))
+(def node-array (atom []))
 
 (defn pop-mergeable-stack []
   (let [pop-item (last @mergeable-stack)]
@@ -32,48 +48,49 @@
   (swap! mergeable-stack #(assoc % (count %) item)))
 
 (defn add-internal [item index]
-  (let [array-len (count @peak-array)
+  (let [array-len (count @node-array)
         ;; incidentally correct since index is calculated starting at 1 in lieu of 0
         zero-leaves (- index array-len)]
-    (swap! peak-array concat (repeat zero-leaves 0) (list item))))
+    (swap! node-array concat (repeat zero-leaves 0) (list item))))
 
 (defn reset-all []
  (do
-   (reset! peak-map {})
-   (reset! peak-array [])
+   (reset! node-map {})
+   (reset! node-array [])
    (reset! mergeable-stack [])
    (reset! leaf-count 0)
    (reset! lastP nil)))
 
-(:height (get @peak-map @lastP))
+(:height (get @node-map @lastP))
 (identity @lastP)
 
 (defn hop-left [node]
-  (:left (get @peak-map node)))
+  (:left (get @node-map node)))
 
 (defn hop-parent [node]
-  (:parent (get @peak-map node)))
+  (:parent (get @node-map node)))
 
-(algo)
+
+(algo true)
 (defn algo [upgrade?]
   (let [
         ;; let h be hash of new leaf
         ;; h (str @leaf-count "-hash")
         h #{@leaf-count}
         ;; create object P, set P.hash<-h, set P.height<-0, set P.left<-lastP
-        P (peak-node (:hash (get @peak-map @lastP)) 0 h nil)
+        P (peak-node (:hash (get @node-map @lastP)) 0 h)
         ]
     (do
       ;; 1. Add step
       ;; store object P in peak map
-      (swap! peak-map #(assoc % h P))
+      (swap! node-map #(assoc % h P))
       ;; A[R*n+1]<-h
       (add-internal h (* 2 @leaf-count))
 
       ;; 2. Check mergeable
       ;; if lastP.height==0 then M.add(P)
-      (if (= (:height (get @peak-map @lastP)) 0)
-        (add-mergeable-stack (get @peak-map h)))
+      (if (= (:height (get @node-map @lastP)) 0)
+        (add-mergeable-stack (get @node-map h)))
 
       ;; 3. reset lastP
       (reset! lastP h)
@@ -83,21 +100,23 @@
         (do
           (let [Q (pop-mergeable-stack)
                 Q (update Q :height inc)
-                L (get @peak-map (:left Q))
+                L (get @node-map (:left Q))
                 Q-old-hash (:hash Q)
                 Q (assoc Q :hash (apply sorted-set (concat (:hash L) (:hash Q))))
                 Q (assoc Q :left (:left L))]
-            ;; add new leaf to peak-map
-            (swap! peak-map #(assoc % (:hash Q) Q))
+            ;; add new leaf to node-map
+            (swap! node-map #(assoc % (:hash Q) Q))
             ;; update new parent-values
-            (swap! peak-map #(assoc-in % [(:hash L) :parent] (:hash Q)))
-            (swap! peak-map #(assoc-in % [Q-old-hash :parent] (:hash Q)))
+            (swap! node-map #(assoc-in % [(:hash L) :parent] (:hash Q)))
+            (swap! node-map #(assoc-in % [Q-old-hash :parent] (:hash Q)))
 
-            (swap! peak-map #(assoc-in % [Q-old-hash :parent] (:hash Q)))
+            ;; change type of children to internal
+            (swap! node-map #(assoc-in % [(:hash L) :type] :internal))
+            (swap! node-map #(assoc-in % [Q-old-hash :type] :internal))
 
             (add-internal (:hash Q) (inc (* 2 @leaf-count)))
             ;; issue is that :left of Q can be outdated since may have had subsequent merge
-            (if (= (:height Q) (:height (get @peak-map (:left Q))))
+            (if (= (:height Q) (:height (get @node-map (:left Q))))
               (add-mergeable-stack Q))
             ;; if we've replaced the old lastP, should reset lastP to point to the new entry
             (if (= Q-old-hash @lastP)
@@ -105,31 +124,34 @@
             ;; TODO: the following has a smarter integration
             (if (not upgrade?)
               (do (if (= Q-old-hash (hop-left @lastP))
-                    (swap! peak-map #(assoc-in % [@lastP :left] (:hash Q))))
-                  (if (= Q-old-hash (:left (get @peak-map (:left (get @peak-map @lastP)))))
-                    (swap! peak-map #(assoc-in % [(:left (get @peak-map @lastP)) :left] (:hash Q)))))
+                    (swap! node-map #(assoc-in % [@lastP :left] (:hash Q))))
+                  (if (= Q-old-hash (:left (get @node-map (:left (get @node-map @lastP)))))
+                    (swap! node-map #(assoc-in % [(:left (get @node-map @lastP)) :left] (:hash Q)))))
               (let [
                     left-most-sibling-peak (last (take-while #(and (some? %) (nil? (hop-parent %))) (iterate hop-left @lastP)))
                     correct-sibling-of-left-most (take-while some? (iterate hop-parent (hop-left left-most-sibling-peak)))
                     ]
                 (if (and (some? left-most-sibling-peak) (< 1 (count correct-sibling-of-left-most)))
-                  (swap! peak-map #(assoc-in % [left-most-sibling-peak :left] (last correct-sibling-of-left-most))))))
+                  (swap! node-map #(assoc-in % [left-most-sibling-peak :left] (last correct-sibling-of-left-most))))))
             )
           )
         )
       (swap! leaf-count inc)
+
       ;; 5. TODO update range nodes
 
       ;; check (difference (S-n n) (S-n (dec n)))
       ;; recalculate only those members of S-n that are in the difference set from above
 
-
       ;; show results
-      ;; (clojure.pprint/pprint [@peak-map @peak-array @mergeable-stack @lastP])
-      ;; (clojure.pprint/pprint @peak-map)
-      ;; (clojure.pprint/pprint @peak-map)
+      ;; (clojure.pprint/pprint [@node-map @node-array @mergeable-stack @lastP])
+      ;; (clojure.pprint/pprint @node-map)
+      ;; (clojure.pprint/pprint @node-map)
       ))
   )
+
+(def algo-1222 (play-algo 1222 true))
+(count (filter #(= :peak (:type (val %))) (:node-map algo-1222)))
 
 (algo true)
 (play-algo (last-algo-match) true)
@@ -149,7 +171,7 @@
                           ;; identity
                           )
                          ;; group the peaks by hashset length
-                         (nth (sort (group-by count (keys @peak-map))) n)))))
+                         (nth (sort (group-by count (keys @node-map))) n)))))
         (range (count (core/S-n @leaf-count))))
 
 (def algo-new-mismatch (play-algo (inc (last-algo-match)) true))
@@ -159,18 +181,17 @@
 (:mergeable-stack algo-old-mismatch)
 (:lastP algo-new-mismatch)
 (:lastP algo-old-mismatch)
-(clojure.set/difference (into #{} (:peak-map algo-new-mismatch))
-                        (into #{} (:peak-map algo-old-mismatch)))
-(clojure.set/difference (into #{} (:peak-map algo-old-mismatch))
-                        (into #{} (:peak-map algo-new-mismatch)))
-(@peak-map)
+(clojure.set/difference (into #{} (:node-map algo-new-mismatch))
+                        (into #{} (:node-map algo-old-mismatch)))
+(clojure.set/difference (into #{} (:node-map algo-old-mismatch))
+                        (into #{} (:node-map algo-new-mismatch)))
 
 (defn play-algo [n upgrade?]
   (do (reset-all)
       (doall (repeatedly n #(algo upgrade?)))
       ;; (println "-----------------")
-      ;; (clojure.pprint/pprint @peak-map)
-      {:peak-map @peak-map
+      ;; (clojure.pprint/pprint @node-map)
+      {:node-map @node-map
        :lastP @lastP
        :mergeable-stack @mergeable-stack}
       ))
@@ -178,15 +199,17 @@
 ;; test that that tree construction is correct
 (let [n 1222
       nodes (play-algo n true)
-      parent-less (filter #(= nil (:parent (val %))) (:peak-map nodes))]
-  (= (reverse (sort (map (comp :height val) parent-less))) (storage/S-n n))
-  (= (reverse (map (comp :height #(get @peak-map %)) (take-while some? (iterate hop-left (:lastP nodes))))) (storage/S-n n))
-  )
-(every? nil? (map #(:parent (get @peak-map %)) (take-while #(some? (get @peak-map %)) (iterate hop-left @lastP))))
-
+      parent-less (filter #(= nil (:parent (val %))) (:node-map nodes))]
+  (every? true?
+          [
+           (= (storage/S-n n) (reverse (sort (map (comp :height val) parent-less))))
+           (= (storage/S-n n) (reverse (map (comp :height #(get @node-map %)) (take-while some? (iterate hop-left (:lastP nodes))))))
+           (every? nil? (map #(:parent (get @node-map %)) (take-while #(some? (get @node-map %)) (iterate hop-left @lastP))))
+           ]
+          ))
 
 (defn last-algo-match
-  "plays algo while the upgrade and old algos still match"
+  "plays algo while the upgrade and old algo still match"
   []
   (last (take-while
     #(let [
@@ -209,9 +232,9 @@
 (do
   ;; (play-algo 10 false)
   (play-algo 0 false)
-  (map (fn [[k v]] [k (:parent v)]) @peak-map)
-  (keys @peak-map)
+  (map (fn [[k v]] [k (:parent v)]) @node-map)
+  (keys @node-map)
   )
 
-(keys @peak-map)
-(get @peak-map #{8 9})
+(keys @node-map)
+(get @node-map #{8 9})
