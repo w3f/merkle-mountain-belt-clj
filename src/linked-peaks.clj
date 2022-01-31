35ab1834cfb9e1bfd4075cbdd7c1482f8b06e305
Robert Hambrock <roberthambrock@gmail.com>
2022-01-31 12:38:56 +0000

    verify that b_j == 1 and that table 4 from draft holds

--- src/linked-peaks.clj
+++ src/linked-peaks.clj
@@ -205,8 +205,50 @@
       ))
   )
 
-(some? (play-algo 1222 false))
+;; test: bj always 1
+(every? #(= "1" %)
+        (map
+         #(let [b-reverse (map str (reverse (storage/binary-repr-of-n (inc %))))
+                j (storage/p-adic-order 2 (inc %))]
+            (nth b-reverse j))
+         (range 1500)))
+
+(let [n 1339
+      b (storage/binary-repr-of-n (inc n))
+      ;; [b1 b0] (map #(Integer/parseInt %) (map str (drop (- (count b) 2) b)))
+      j (storage/p-adic-order 2 (inc n))
+      m1 (if (< 1 j) "new leaf joins last range"
+             (get {0 "new leaf participates in merge"
+                   1 "new leaf forms a range alone"} j))
+      [bj+2 bj+1 bj] (map #(Integer/parseInt (str %)) (map #(nth (reverse b) %) [(+ j 2) (+ j 1) j]))
+      m2 (if (= [bj+1 bj] [0 1])
+           "M.M. not alone in range"
+           (if (= [bj+2 bj+1 bj] [0 1 1])
+             "M.M. alone in range"
+             (if (= [bj+2 bj+1 bj] [1 1 1])
+               "M.M. joins prev range"
+               )))
+      ;; {
+      ;;  {
+      ;;   1 "new leaf participates in merge"
+      ;;   0 {:b1 {
+      ;;           0 "new leaf joins last range"
+      ;;           1 "new leaf formas a range alone"
+      ;;           }}
+      ;;   }
+      ;;  :j {}
+      ;;  }
+      ;; j-map {0
+      ;;        }
+      ]
+  [m1 m2]
+  ;; [b1 b0 j]
+
+  )
+
+(some? (play-algo 1222 true))
 (def algo-1222 (play-algo 1222 true))
+(def algo-1223 (play-algo 1223 true))
 (def algo-1277 (play-algo 1277 true))
 (def algo-1278 (play-algo 1278 true))
 (def algo-1279 (play-algo 1279 true))
