d79b10e0d3897c5b54a514f23d58122237d4d1f8
Robert Hambrock <roberthambrock@gmail.com>
2024-06-20 13:55:37 +0200

    proof size: get position of range

--- src/proof_size.clj
+++ src/proof_size.clj
@@ -10,6 +10,18 @@
                  [[[]] 0 -1]
                  S-n)))
 
+(defn range-position [m ranges acc-ranges acc-leaves]
+  (let [range-splits ranges
+        last-range-max-m (reduce (fn [acc elem] (+ acc (Math/pow 2 elem))) 0 (peek range-splits))
+        acc-leaves (+ acc-leaves last-range-max-m)]
+    (if (<= m acc-leaves)
+      acc-ranges
+      (range-position m (pop range-splits) (inc acc-ranges) acc-leaves)
+      )
+    ))
+
+(range-position 80 (range-splits (p/S-n 2000)) 0 0)
+
 (let [n 2000
       S-n (p/S-n n)
       range-splits (range-splits S-n)]
