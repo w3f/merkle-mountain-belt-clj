7d50938db4266ade92ac8bff9f39820519ae258d
Robert Hambrock <roberthambrock@gmail.com>
2024-06-20 14:47:39 +0200

    calculate nested range position

--- src/proof_size.clj
+++ src/proof_size.clj
@@ -14,12 +14,40 @@
   (let [last-range-max-m (+ acc-leaves (Math/pow 2 (last S-n)))]
     (if (< m last-range-max-m)
       current-range
-      (range-position-flat m (butlast S-n) (+ acc-leaves last-range-max-m) (inc current-range)))))
+      (range-position-flat m (butlast S-n) last-range-max-m (inc current-range)))))
 
+(defn range-position-nested [m S-n]
+  (let [range-position (range-position-flat m S-n 0 0)
+        range-splits (range-splits S-n)
+        ]
+    (letfn [(range-position-nested-internal [range-position range-splits acc-ranges]
+              (let [last-range-length (count (peek range-splits))]
+                   (if (< range-position last-range-length)
+                     [acc-ranges range-position]
+                     (range-position-nested-internal (- range-position last-range-length) (pop range-splits) (inc acc-ranges))))
+              )]
+      (range-position-nested-internal range-position range-splits 0)
       )
     ))
 
-(range-position 80 (range-splits (p/S-n 2000)) 0 0)
+(range-position-nested 150 (p/S-n 2000))
+
+(let [m 3
+      n 2000
+      S-n (p/S-n n)
+      range-splits (range-splits S-n)
+      range-position-flat (range-position-flat m S-n 0 0)
+      range-position-nested (range-position-nested m S-n)
+      surrounding-range (nth range-splits (- (dec (count range-splits)) (first range-position-nested)))]
+  [range-splits
+   {:pos range-position-flat :height (nth S-n (- (dec (count S-n)) range-position-flat))}
+   {:pos range-position-nested :height (nth surrounding-range (- (dec (count surrounding-range)) (second range-position-nested)))}
+  (=
+   (nth S-n (- (dec (count S-n)) range-position-flat))
+   (nth (nth range-splits (- (dec (count range-splits)) (first range-position-nested)))
+        (- (dec (count surrounding-range)) (second range-position-nested))))
+   ]
+  )
 
 (let [n 2000
       S-n (p/S-n n)
