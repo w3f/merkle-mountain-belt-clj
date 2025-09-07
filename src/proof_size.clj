062bbc7842eb16b9b0e4d3b73400cf735b856a51
Robert Hambrock <roberthambrock@gmail.com>
2025-09-07 08:12:49 +0200

    dump stats to disk

--- src/proof_size.clj
+++ src/proof_size.clj
@@ -38,11 +38,16 @@
       )
     ))
 
-(defn proof-size [n m]
+(defn avg [coll]
+  (/ (reduce + coll) (count coll)))
+
+(defn proof-size
+  "returns the proof size for an MMB size `n` with leaf depth `k`"
+ [n k]
    (let [S-n (p/S-n n)
          range-splits (range-splits S-n)
-         range-position-flat (range-position-flat m S-n 0 0)
-         range-position-nested (range-position-nested m S-n)
+         range-position-flat (range-position-flat k S-n 0 0)
+         range-position-nested (range-position-nested k S-n)
          peak-height (nth-reverse S-n range-position-flat)
          aggregate-for-left-range-nodes (if (< (second range-position-nested)
                                                (dec (count (nth-reverse range-splits (first range-position-nested)))))
@@ -58,17 +63,25 @@
      ;;  :agg (apply + (flatten [peak-height range-position-nested aggregate-for-left-range-nodes aggregate-for-left-belt-nodes]))}
    (apply + (flatten [peak-height range-position-nested aggregate-for-left-range-nodes aggregate-for-left-belt-nodes]))))
 
+(defn spit-lazy-to-file [lazy file append]
+  (spit file (with-out-str (doseq [i (doall lazy)] (println i))) :append append))
+
+(spit-lazy-to-file
+ (pmap (fn [k] {(inc k) (float (avg (map #(proof-size % k)
+                                        (apply range (let [base (Math/pow 2 24)] [base (+ base (Math/pow 2 20))])))))})
+       (map dec [1 10 50 200 600]))
+ "stats/absolute-proof-size.dat" false)
 
 ;; loop for checking that proof-size calculation matches implementation
 (comment
   (doall (repeatedly 150 #(algo false)))
   ;; append to file
   (doall (repeatedly 2000 #(do (algo false)
-                               (spit "leaf-count.dat" (str  [@leaf-count (every? true? (pmap (fn [m] (let [n @leaf-count
+                               (spit "stats/leaf-count.dat" (str  [@leaf-count (every? true? (pmap (fn [m] (let [n @leaf-count
                                                                                                           expected-proof-size (proof-size n (- n m))
                                                                                                           actual-proof-size (count (proof/co-path-internal (primitives.storage/leaf-location m) [] nil true))]
                                                                                                       (= expected-proof-size actual-proof-size)))
-                                                                                             (range 1 151)))]) :append true)
+                                                                                             (range 1 151)))]) "append" true)
                                ))))
 
 (every? true? (map (fn [m] (let [n 100
@@ -81,41 +94,41 @@
       blocks-per-year (* blocks-per-minute 60 24 365)
       n-range-min (* 2 blocks-per-year)
       n-range-max (+ n-range-min (* 5 blocks-per-year))
-      n-max 10000
-      k 150
+      n-max (Math/pow 2 11)
+      k 2400
       step-size 1000000]
-  (spit "avg-proof-size.dat" (with-out-str (doseq [i (doall (pmap (fn [n-min] {:n-min n-min
-                                                                              :avg-proof-sizes (let [n-max (+ n-min n-max)]
-                                                                                                 (-> (->> (map (fn [n] (map (fn [m] (proof-size n m))
-                                                                                                                           (range 0 k))) (range n-min n-max))
-                                                                                                          (flatten)
-                                                                                                          (reduce +))
-                                                                                                     (/ (* k (- n-max n-min)))
-                                                                                                     (float)
-                                                                                                     ))
-                                                                              }
-                                                                    )
-                                                                  (range n-range-min n-range-max step-size)))]
-                                             (print i "\n"))) :append false))
+  (spit (str "stats/avg-proof-size-" k ".dat") (with-out-str (doseq [i (doall (pmap (fn [n-min] {:n-min n-min
+                                                                                    :avg-proof-sizes (let [n-max (+ n-min n-max)]
+                                                                                                       (-> (->> (map (fn [n] (map (fn [m] (proof-size n m))
+                                                                                                                                 (range 0 k))) (range n-min n-max))
+                                                                                                                (flatten)
+                                                                                                                (reduce +))
+                                                                                                           (/ (* k (- n-max n-min)))
+                                                                                                           (float)
+                                                                                                           ))
+                                                                                    }
+                                                                          )
+                                                                        (range n-range-min n-range-max step-size)))]
+                                                   (print i "\n"))) :append false))
 
 (do (clojure.test/deftest test-range-splits
       (clojure.test/is
-          (every? true? (map (fn [n m] (let [S-n (p/S-n n)
-                                  range-splits (range-splits S-n)
-                                  range-position-flat (range-position-flat m S-n 0 0)
-                                  range-position-nested (range-position-nested m S-n)
-                                  surrounding-range (nth range-splits (- (dec (count range-splits)) (first range-position-nested)))]
-                              ;; [range-splits
-                              ;;  {:pos range-position-flat :height (nth S-n (- (dec (count S-n)) range-position-flat))}
-                              ;;  {:pos range-position-nested :height (nth surrounding-range (- (dec (count surrounding-range)) (second range-position-nested)))}
-                              ;;  ]
-                              (=
-                               (nth S-n (- (dec (count S-n)) range-position-flat))
-                               (nth (nth range-splits (- (dec (count range-splits)) (first range-position-nested)))
-                                    (- (dec (count surrounding-range)) (second range-position-nested))))
-                              ))
-                   (take 200000 (repeat 20000000)) (range 1 200000)))
-          ))
+       (every? true? (map (fn [n m] (let [S-n (p/S-n n)
+                                         range-splits (range-splits S-n)
+                                         range-position-flat (range-position-flat m S-n 0 0)
+                                         range-position-nested (range-position-nested m S-n)
+                                         surrounding-range (nth range-splits (- (dec (count range-splits)) (first range-position-nested)))]
+                                     ;; [range-splits
+                                     ;;  {:pos range-position-flat :height (nth S-n (- (dec (count S-n)) range-position-flat))}
+                                     ;;  {:pos range-position-nested :height (nth surrounding-range (- (dec (count surrounding-range)) (second range-position-nested)))}
+                                     ;;  ]
+                                     (=
+                                      (nth S-n (- (dec (count S-n)) range-position-flat))
+                                      (nth (nth range-splits (- (dec (count range-splits)) (first range-position-nested)))
+                                           (- (dec (count surrounding-range)) (second range-position-nested))))
+                                     ))
+                          (take 200000 (repeat 20000000)) (range 1 200000)))
+       ))
     (clojure.test/run-test test-range-splits))
 
 (let [n 2000
