8405615d10c10913d6060a97720c4004f1d43812
Robert Hambrock <roberthambrock@gmail.com>
2026-07-01 14:13:17 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -2,30 +2,41 @@
   description = "Merkle Mountain Belt Cloure";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-26.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem
-    (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in {
-      devShells.default = pkgs.mkShell {
-         buildInputs = [
-          pkgs.clojure
-          pkgs.graphviz
-         ];
-         shellHook = ''
-           echo "Welcome to Clojure!"
-           echo "Run 'clojure' to start the REPL"
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.jdk21
+            pkgs.clojure
+            pkgs.clojure-lsp
+            pkgs.git
+          ];
+          buildInputs = [
+            pkgs.clojure
+            pkgs.clojure-lsp
+            pkgs.graphviz
+          ];
+          shellHook = ''
+            echo "Welcome to Clojure!"
+            echo "Run 'clojure' to start the REPL"
           '';
-      };
-    });
+        };
+      }
+    );
 }
